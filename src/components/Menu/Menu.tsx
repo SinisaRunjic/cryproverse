b8594b6e7bb079aea248a4089c4f249d73195f10
SinisaRunjic <sinisa.runjic@gmail.com>
2023-12-21 12:23:59 +0100

    add function in nav bar

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,31 +1,13 @@
-import { FC, useState } from "react";
+import { FC } from "react";
 import { Menu as AntdMenu } from "antd";
 import { IMenu } from "./interface";
-import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
-import { Button } from "components";
 
 const Menu: FC<IMenu> = ({ ...props }) => {
- const [collapsed, setCollapsed] = useState(false);
-
- const toggleCollapsed = () => {
-  setCollapsed(!collapsed);
- };
- return (
-  <>
-   <Button
-    type="primary"
-    onClick={toggleCollapsed}
-    style={{ marginBottom: 16 }}
-   >
-    {collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-   </Button>
-   <AntdMenu {...props} />
-  </>
- );
+  return <AntdMenu {...props} />;
 };
 
 Menu.defaultProps = {
- mode: "inline",
+  mode: "inline",
 };
 
 export default Menu;
