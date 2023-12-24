cb4f8df50393f9d0724e6013e7cda565240ed90d
SinisaRunjic <sinisa.runjic@gmail.com>
2023-12-24 15:49:04 +0100

    add some small changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,26 @@ import "./App.css";
 import { Cryptocurrencies, Exchanges, Home, News, NotFound } from "pages";
 
 const App: React.FC = () => {
-  return (
-    <Layout hasSider>
-      <NavBar />
-      <Layout>
-        <Header>Header</Header>
-        <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
-            <Route path="/exchanges" element={<Exchanges />} />
-            <Route path="/news" element={<News />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>
-          Ant Design ©2023 Created by Ant UED
-        </Footer>
-      </Layout>
-    </Layout>
-  );
+ return (
+  <Layout hasSider>
+   <NavBar />
+   <Layout style={{ marginLeft: 200 }}>
+    <Header>Header</Header>
+    <Content style={{ margin: "24px 16px 0", overflow: "initial" }}>
+     <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+      <Route path="/exchanges" element={<Exchanges />} />
+      <Route path="/news" element={<News />} />
+      <Route path="*" element={<NotFound />} />
+     </Routes>
+    </Content>
+    <Footer style={{ textAlign: "center" }}>
+     Ant Design ©2023 Created by Ant UED
+    </Footer>
+   </Layout>
+  </Layout>
+ );
 };
 
 export default App;
